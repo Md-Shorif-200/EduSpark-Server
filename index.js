aa08cf37633f2e4c94a73bf7a01394cb91cec93f
md shorif <mdshorifuddin463@gmail.com>
2025-02-14 22:05:25 +0600

    post & get users data

--- index.js
+++ index.js
@@ -31,7 +31,28 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    
+    // !database collection
+    const userCollection = client.db('academixDb').collection('Users');
+
+
+
+
+    // ! user related  Api
+
+    app.post('/users' , async(req,res) => {
+          const user = req.body;
+          const  result =  await userCollection.insertOne(user);
+          res.send(result)
+    })
+
+    app.get('/users', async(req,res) => {
+         const result = await userCollection.find().toArray();
+         res.send(result)
+    })
+
+
+
+
     
 
 
